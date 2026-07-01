38ceb07b1b8fcfcfa441c3db82f4a5371688ef2b
riyanshah112008-debug <riyanshah112008@gmail.com>
2026-07-01 16:35:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ loadModule('Translator', './translator.js');
 
 // FIX: Match these paths EXACTLY to your filenames on GitHub (Check for Capital Letters!)
 loadModule('Reaction Roles', './reactionRoles.js'); 
-loadModule('Canvas Image Gen', './canvasimagegen.js'); 
+loadModule('Canvas Image Gen', './imageGen.js'); 
 
 loadModule('Music', './music.js');
 loadModule('Help', './help.js');
