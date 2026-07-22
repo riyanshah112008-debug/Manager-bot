e4a401184386778811e69e0d4abb7bd71e051464
riyanshah112008-debug <riyanshah112008@gmail.com>
2026-07-22 16:34:01 +0530

    Update deploy-commands.js

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -85,6 +85,26 @@ const commands = [
     { name: 'delwarn', description: 'Delete a warning by its ID', default_member_permissions: MANAGE_MESSAGES, options: [
         { name: 'id', type: 4, required: true, description: 'Warning ID' }
     ] },
+    //---------------------AUTOROLE--------------------
+    {
+    name: 'autorole',
+    description: 'Configure the autorole and automatic role backup system',
+    default_member_permissions: String(PermissionFlagsBits.Administrator),
+    options: [
+        {
+            name: 'role',
+            description: 'The default role to automatically give to new members',
+            type: 8, 
+            required: false
+        },
+        {
+            name: 'sticky_roles',
+            description: 'Should the bot automatically save and restore roles when people leave/rejoin?',
+            type: 5, 
+            required: false
+        }
+    ]
+},
     
     // ================= AUTOMOD =================
     { name: 'automod', description: 'Configure the server-wide automod switch', default_member_permissions: ADMIN, options: [
