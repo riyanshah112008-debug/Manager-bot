b75d494e8ccdbf5606353288b386ca31b1e0d992
riyanshah112008-debug <riyanshah112008@gmail.com>
2026-07-25 22:07:37 +0530

    Create kiss.js

---
+++ src/commands/economy/kiss.js
@@ -0,0 +1,97 @@
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const User = require('../models/User');
+
+const KISS_GIFS = [
+    'https://media1.tenor.com/m/gzaT07Fk4UoAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/F02Ep3b_dIgAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/etSTc3aWspcAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/lYHV1vwa-FkAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/I8kWjuAtX-QAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/I9Z44UqA4UIAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/PZc3XgM-a5IAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/1Gj23LpA7WMAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/QfL2Piv3K3wAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/h5e17uVzL7MAAAAC/anime-kiss.gif'
+];
+
+async function trackKiss(userId, guildId, isGiven) {
+    if (!userId) return;
+    const updateField = isGiven ? { kissesGiven: 1 } : { kissesReceived: 1 };
+    try {
+        await User.findOneAndUpdate(
+            { userId: userId, guildId: guildId },
+            { $inc: updateField },
+            { upsert: true, new: true }
+        );
+    } catch (err) {
+        console.error('DB Kiss Track Error:', err);
+    }
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('kiss')
+        .setDescription('💋 Give someone a sweet anime kiss (Works in DMs too!)')
+        .setContexts([0, 1, 2])
+        .setIntegrationTypes([0, 1])
+        .addUserOption(option => 
+            option.setName('target')
+                .setDescription('The user you want to kiss')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        await interaction.deferReply(); // Prevents Discord 3-second timeout error
+
+        const target = interaction.options.getUser('target');
+        const guildId = interaction.guildId || 'DM';
+
+        await trackKiss(interaction.user.id, guildId, true);
+        await trackKiss(target.id, guildId, false);
+
+        const randomGif = KISS_GIFS[Math.floor(Math.random() * KISS_GIFS.length)];
+        const embed = new EmbedBuilder()
+            .setColor('#FFB6C1')
+            .setDescription(`💋 **${interaction.user.username}** kissed **${target.username}**!`)
+            .setImage(randomGif);
+
+        const row = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setCustomId('social_kiss_back')
+                .setLabel('Kiss back')
+                .setStyle(ButtonStyle.Secondary)
+                .setEmoji('💋')
+        );
+
+        const components = (target.id === interaction.user.id || target.bot) ? [] : [row];
+        const response = await interaction.editReply({ embeds: [embed], components: components });
+
+        if (components.length === 0) return;
+
+        const collector = response.createMessageComponentCollector({ time: 60000 });
+
+        collector.on('collect', async (i) => {
+            if (i.user.id !== target.id) {
+                return i.reply({ content: 'Only the person who was kissed can kiss back!', ephemeral: true });
+            }
+
+            await trackKiss(target.id, guildId, true);
+            await trackKiss(interaction.user.id, guildId, false);
+
+            const returnGif = KISS_GIFS[Math.floor(Math.random() * KISS_GIFS.length)];
+            const returnEmbed = new EmbedBuilder()
+                .setColor('#FFB6C1')
+                .setDescription(`💋 **${target.username}** kissed **${interaction.user.username}** back!`)
+                .setImage(returnGif);
+
+            row.components[0].setDisabled(true);
+            await interaction.editReply({ components: [row] }).catch(() => {});
+            await i.reply({ embeds: [returnEmbed] });
+        });
+
+        collector.on('end', () => {
+            row.components[0].setDisabled(true);
+            interaction.editReply({ components: [row] }).catch(() => {});
+        });
+    }
+};
