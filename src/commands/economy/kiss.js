b8f6335593afd5b5d3ed9eca5e1b4268f4c945ee
riyanshah112008-debug <riyanshah112008@gmail.com>
2026-07-25 20:58:34 +0530

    Update kiss.js

--- src/commands/economy/kiss.js
+++ src/commands/economy/kiss.js
@@ -1,23 +1,18 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-const https = require('https');
 const User = require('../models/User');
 
-function getGif() {
-    return new Promise((resolve) => {
-        https.get('https://api.waifu.pics/sfw/kiss', (res) => {
-            let data = '';
-            res.on('data', chunk => data += chunk);
-            res.on('end', () => {
-                try {
-                    const json = JSON.parse(data);
-                    resolve(json.url);
-                } catch {
-                    resolve('https://i.imgur.com/13w1J4L.png');
-                }
-            });
-        }).on('error', () => resolve('https://i.imgur.com/13w1J4L.png'));
-    });
-}
+const KISS_GIFS = [
+    'https://media1.tenor.com/m/gzaT07Fk4UoAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/F02Ep3b_dIgAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/etSTc3aWspcAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/lYHV1vwa-FkAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/I8kWjuAtX-QAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/I9Z44UqA4UIAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/PZc3XgM-a5IAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/1Gj23LpA7WMAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/QfL2Piv3K3wAAAAC/anime-kiss.gif',
+    'https://media1.tenor.com/m/h5e17uVzL7MAAAAC/anime-kiss.gif'
+];
 
 function trackKiss(userId, guildId, isGiven) {
     if (!userId) return;
@@ -26,7 +21,7 @@ function trackKiss(userId, guildId, isGiven) {
         { userId: userId, guildId: guildId },
         { $inc: updateField },
         { upsert: true, new: true }
-    ).catch(err => console.error('DB Kiss Track Error:', err));
+    ).catch(() => {});
 }
 
 module.exports = {
@@ -42,18 +37,17 @@ module.exports = {
         ),
 
     async execute(interaction) {
-        await interaction.deferReply();
         const target = interaction.options.getUser('target');
         const guildId = interaction.guildId || 'DM';
 
-        const gifUrl = await getGif();
         trackKiss(interaction.user.id, guildId, true);
         trackKiss(target.id, guildId, false);
 
+        const randomGif = KISS_GIFS[Math.floor(Math.random() * KISS_GIFS.length)];
         const embed = new EmbedBuilder()
             .setColor('#FFB6C1')
             .setDescription(`💋 **${interaction.user.username}** kissed **${target.username}**!`)
-            .setImage(gifUrl);
+            .setImage(randomGif);
 
         const row = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
@@ -64,7 +58,7 @@ module.exports = {
         );
 
         const components = (target.id === interaction.user.id || target.bot) ? [] : [row];
-        const response = await interaction.editReply({ embeds: [embed], components: components });
+        const response = await interaction.reply({ embeds: [embed], components: components });
 
         if (components.length === 0) return;
 
@@ -78,7 +72,7 @@ module.exports = {
             trackKiss(target.id, guildId, true);
             trackKiss(interaction.user.id, guildId, false);
 
-            const returnGif = await getGif();
+            const returnGif = KISS_GIFS[Math.floor(Math.random() * KISS_GIFS.length)];
             const returnEmbed = new EmbedBuilder()
                 .setColor('#FFB6C1')
                 .setDescription(`💋 **${target.username}** kissed **${interaction.user.username}** back!`)
