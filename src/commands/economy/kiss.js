5260fe74dc6a68117be502e78e04120b91e52d06
riyanshah112008-debug <riyanshah112008@gmail.com>
2026-07-25 20:48:08 +0530

    Update kiss.js

--- src/commands/economy/kiss.js
+++ src/commands/economy/kiss.js
@@ -1,22 +1,33 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const https = require('https');
+const User = require('../models/User');
 
-const KISS_GIFS = [
-    'https://media1.tenor.com/m/gzaT07Fk4UoAAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/F02Ep3b_dIgAAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/etSTc3aWspcAAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/lYHV1vwa-FkAAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/I8kWjuAtX-QAAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/I9Z44UqA4UIAAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/PZc3XgM-a5IAAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/1Gj23LpA7WMAAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/QfL2Piv3K3wAAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/h5e17uVzL7MAAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/nEID0Jro6V8AAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/5J3mEaWpCMAAAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/dpv2qQf-J8cAAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/7sZ60E5g56IAAAAC/anime-kiss.gif',
-    'https://media1.tenor.com/m/X8B2hH861u8AAAAC/anime-kiss.gif'
-];
+function getGif() {
+    return new Promise((resolve) => {
+        https.get('https://api.waifu.pics/sfw/kiss', (res) => {
+            let data = '';
+            res.on('data', chunk => data += chunk);
+            res.on('end', () => {
+                try {
+                    const json = JSON.parse(data);
+                    resolve(json.url);
+                } catch {
+                    resolve('https://i.imgur.com/13w1J4L.png');
+                }
+            });
+        }).on('error', () => resolve('https://i.imgur.com/13w1J4L.png'));
+    });
+}
+
+async function trackKiss(userId, guildId, isGiven) {
+    if (!userId) return;
+    const updateField = isGiven ? { kissesGiven: 1 } : { kissesReceived: 1 };
+    await User.findOneAndUpdate(
+        { userId: userId, guildId: guildId },
+        { $inc: updateField },
+        { upsert: true, new: true }
+    ).catch(err => console.error('DB Kiss Track Error:', err));
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -31,13 +42,18 @@ module.exports = {
         ),
 
     async execute(interaction) {
+        await interaction.deferReply();
         const target = interaction.options.getUser('target');
-        const randomGif = KISS_GIFS[Math.floor(Math.random() * KISS_GIFS.length)];
+        const guildId = interaction.guildId || 'DM';
+        const gifUrl = await getGif();
+
+        await trackKiss(interaction.user.id, guildId, true);
+        await trackKiss(target.id, guildId, false);
 
         const embed = new EmbedBuilder()
             .setColor('#FFB6C1')
             .setDescription(`💋 **${interaction.user.username}** kissed **${target.username}**!`)
-            .setImage(randomGif);
+            .setImage(gifUrl);
 
         const row = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
@@ -48,7 +64,7 @@ module.exports = {
         );
 
         const components = (target.id === interaction.user.id || target.bot) ? [] : [row];
-        const response = await interaction.reply({ embeds: [embed], components: components });
+        const response = await interaction.editReply({ embeds: [embed], components: components });
 
         if (components.length === 0) return;
 
@@ -59,7 +75,10 @@ module.exports = {
                 return i.reply({ content: 'Only the person who was kissed can kiss back!', ephemeral: true });
             }
 
-            const returnGif = KISS_GIFS[Math.floor(Math.random() * KISS_GIFS.length)];
+            await trackKiss(target.id, guildId, true);
+            await trackKiss(interaction.user.id, guildId, false);
+
+            const returnGif = await getGif();
             const returnEmbed = new EmbedBuilder()
                 .setColor('#FFB6C1')
                 .setDescription(`💋 **${target.username}** kissed **${interaction.user.username}** back!`)
