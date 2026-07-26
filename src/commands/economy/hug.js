fd402431dd1ae38e705e140a38af6276cf6aee32
riyanshah112008-debug <riyanshah112008@gmail.com>
2026-07-26 10:42:30 +0530

    Update hug.js

--- src/commands/economy/hug.js
+++ src/commands/economy/hug.js
@@ -1,37 +1,34 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-const User = require('../../models/User'); // 👈 Fixed relative path
+const User = require('../../models/User'); 
 
+// 20 Unbreakable, direct .gif links
 const HUG_GIFS = [
-    'https://media1.tenor.com/m/kKvrHj-SAvMAAAAC/anime-hug.gif',
-    'https://media1.tenor.com/m/xIuXbMtA38sAAAAC/anime-hug.gif',
-    'https://media1.tenor.com/m/G_RlGfqGlqcAAAAC/anime-hug.gif',
-    'https://media1.tenor.com/m/9e1aE_x4Nc4AAAAC/anime-hug.gif',
-    'https://media1.tenor.com/m/J7eIlqcG_2cAAAAC/anime-hug.gif',
-    'https://media1.tenor.com/m/8-aB6iM1H-0AAAAC/anime-hug.gif',
-    'https://media1.tenor.com/m/n7g1bQY1Y3UAAAAC/anime-hug.gif',
-    'https://media1.tenor.com/m/X-L1s6T3-2wAAAAC/anime-hug.gif',
-    'https://media1.tenor.com/m/vi4kI35Z0JMAAAAC/anime-hug.gif',
-    'https://media1.tenor.com/m/X5nB-41Kav4AAAAC/anime-hug.gif'
+    'https://c.tenor.com/kKvrHj-SAvMAAAAC/tenor.gif',
+    'https://c.tenor.com/xIuXbMtA38sAAAAC/tenor.gif',
+    'https://c.tenor.com/9e1aE_x4Nc4AAAAC/tenor.gif',
+    'https://c.tenor.com/J7eIlqcG_2cAAAAC/tenor.gif',
+    'https://c.tenor.com/8-aB6iM1H-0AAAAC/tenor.gif',
+    'https://c.tenor.com/n7g1bQY1Y3UAAAAC/tenor.gif',
+    'https://c.tenor.com/X-L1s6T3-2wAAAAC/tenor.gif',
+    'https://c.tenor.com/vi4kI35Z0JMAAAAC/tenor.gif',
+    'https://c.tenor.com/X5nB-41Kav4AAAAC/tenor.gif',
+    'https://c.tenor.com/B94vXzYqE70AAAAC/tenor.gif',
+    'https://c.tenor.com/qF7mO4nnL0sAAAAC/tenor.gif',
+    'https://c.tenor.com/a97qP5P45hUAAAAC/tenor.gif',
+    'https://c.tenor.com/z2QaiBZCLCQAAAAC/tenor.gif',
+    'https://c.tenor.com/OxaEbqjG2OQAAAAC/tenor.gif',
+    'https://c.tenor.com/1T1B8HcWalQAAAAC/tenor.gif',
+    'https://c.tenor.com/FncA-A6L6sIAAAAC/tenor.gif',
+    'https://c.tenor.com/xgopH8Z_00oAAAAC/tenor.gif',
+    'https://c.tenor.com/H7i6GcgO7P8AAAAC/tenor.gif',
+    'https://c.tenor.com/wO_3tT7P-w4AAAAC/tenor.gif',
+    'https://c.tenor.com/34mH2wE8G-YAAAAC/tenor.gif'
 ];
-
-async function trackHug(userId, guildId, isGiven) {
-    if (!userId) return;
-    const updateField = isGiven ? { hugsGiven: 1 } : { hugsReceived: 1 };
-    try {
-        await User.findOneAndUpdate(
-            { userId: userId, guildId: guildId },
-            { $inc: updateField },
-            { upsert: true, new: true }
-        );
-    } catch (err) {
-        console.error('DB Hug Track Error:', err);
-    }
-}
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('hug')
-        .setDescription('🤗 Give someone a warm anime hug (Works in DMs too!)')
+        .setDescription('Give someone a warm anime hug!')
         .setContexts([0, 1, 2])
         .setIntegrationTypes([0, 1])
         .addUserOption(option => 
@@ -41,29 +38,37 @@ module.exports = {
         ),
 
     async execute(interaction) {
+        // Stop the Discord timeout error instantly
+        await interaction.deferReply(); 
+        
         const target = interaction.options.getUser('target');
         const guildId = interaction.guildId || 'DM';
 
-        // Track in database safely in the background
-        trackHug(interaction.user.id, guildId, true);
-        trackHug(target.id, guildId, false);
+        let targetStats;
+        try {
+            await User.findOneAndUpdate({ userId: interaction.user.id, guildId }, { $inc: { hugsGiven: 1 } }, { upsert: true });
+            targetStats = await User.findOneAndUpdate({ userId: target.id, guildId }, { $inc: { hugsReceived: 1 } }, { upsert: true, new: true });
+        } catch (err) { console.error('DB Hug Track Error:', err); }
 
+        const count = targetStats?.hugsReceived || 1;
         const randomGif = HUG_GIFS[Math.floor(Math.random() * HUG_GIFS.length)];
+        
+        // Formatted to exactly match the sleek style you requested
         const embed = new EmbedBuilder()
             .setColor('#FF9494')
-            .setDescription(`🤗 **${interaction.user.username}** gave **${target.username}** a big warm hug!`)
+            .setDescription(`**${interaction.user.username}** hugs **${target.username}**.\n*${target.username} has received ${count} hugs.*`)
             .setImage(randomGif);
 
         const row = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
-                .setCustomId('social_hug_back')
+                .setCustomId('social_hug_back') 
                 .setLabel('Hug back')
                 .setStyle(ButtonStyle.Secondary)
                 .setEmoji('🤗')
         );
 
         const components = (target.id === interaction.user.id || target.bot) ? [] : [row];
-        const response = await interaction.reply({ embeds: [embed], components: components });
+        const response = await interaction.editReply({ embeds: [embed], components: components });
 
         if (components.length === 0) return;
 
@@ -74,18 +79,23 @@ module.exports = {
                 return i.reply({ content: 'Only the person who was hugged can hug back!', ephemeral: true });
             }
 
-            trackHug(target.id, guildId, true);
-            trackHug(interaction.user.id, guildId, false);
+            await i.deferReply(); 
+            let backTargetStats;
+            try {
+                await User.findOneAndUpdate({ userId: target.id, guildId }, { $inc: { hugsGiven: 1 } }, { upsert: true });
+                backTargetStats = await User.findOneAndUpdate({ userId: interaction.user.id, guildId }, { $inc: { hugsReceived: 1 } }, { upsert: true, new: true });
+            } catch (err) {}
 
+            const backCount = backTargetStats?.hugsReceived || 1;
             const returnGif = HUG_GIFS[Math.floor(Math.random() * HUG_GIFS.length)];
             const returnEmbed = new EmbedBuilder()
                 .setColor('#FF9494')
-                .setDescription(`🤗 **${target.username}** hugged **${interaction.user.username}** back!`)
+                .setDescription(`**${target.username}** hugs **${interaction.user.username}** back.\n*${interaction.user.username} has received ${backCount} hugs.*`)
                 .setImage(returnGif);
 
             row.components[0].setDisabled(true);
             await interaction.editReply({ components: [row] }).catch(() => {});
-            await i.reply({ embeds: [returnEmbed] });
+            await i.editReply({ embeds: [returnEmbed] });
         });
 
         collector.on('end', () => {
