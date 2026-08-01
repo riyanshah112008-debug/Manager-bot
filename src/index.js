e663326f6705e1ba7652d410d82aa75f44a3fcaf
riyanshah112008-debug <riyanshah112008@gmail.com>
2026-08-01 10:32:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -560,8 +560,7 @@ const MODULE_INITIALIZERS = [
     { name: 'Verification System', fn: () => require('./modules/verification.js')(client, app) },
     { name: 'Network Telemetry Engine', fn: () => require('./modules/telemetryEngine.js')(client, app) },
     { name: 'Social Actions Engine', fn: () => require('./modules/socialActions.js')(client, app) },
-    { name: 'Starry Protocol', fn: () => require('./modules/starry.js')(client, app) },
-{ name: 'Master Channel Systems', fn: () => require('./modules/masterChannelSystems.js')(client, app) }
+    { name: 'Starry Protocol',  fn: () => require('./modules/masterChannelSystems.js')(client, app) }
 
 ];
 
