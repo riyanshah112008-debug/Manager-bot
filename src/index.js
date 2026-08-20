a07edee41089eaf2e4bd31a63a2f7600856ee437
riyanshah112008-debug <riyanshah112008@gmail.com>
2026-08-20 20:20:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -551,7 +551,7 @@ client.on(Events.InteractionCreate, async interaction => {
             'bump', 'bump-setup', 'autobump', 'set-listing',
             'ticketsetup', 'applysetup', 'setupcount', 'countstats', 'countreset',
             'whois', 'steal', 'Steal Emojis', 'setwelcome', 'setgoodbye',
-            'setupwelcome', 'setupgoodbye', 'translate', 'clear', 'confessionsetup'
+            'setupwelcome', 'setupgoodbye', 'translate', 'clear', 'confessionsetup','tod'
         ];
         if (moduleHandledCommands.includes(interaction.commandName)) {
             return; 
