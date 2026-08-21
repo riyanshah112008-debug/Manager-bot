681f67adaf851b1c3cc2254a635203c705868955
riyanshah112008-debug <riyanshah112008@gmail.com>
2026-08-21 17:11:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -681,9 +681,8 @@ const MODULE_INITIALIZERS = [
     { name: 'Network Telemetry Engine', fn: () => require('./modules/telemetryEngine.js')(client, app) },
     { name: 'Social Actions Engine', fn: () => require('./modules/socialActions.js')(client, app) },
     { name: 'Anonymous Confession System', fn: () => require('./modules/confession.js')(client, app) }
-];
-
-{ name: 'Truth or Dare', fn: () => require('./modules/truthOrDare.js')(client, app) },
+};
+    { name: 'Truth or Dare', fn: () => require('./modules/truthOrDare.js')(client, app) },
 
 function loadSlashCommands() {
     const commandsPath = path.join(__dirname, 'commands');
